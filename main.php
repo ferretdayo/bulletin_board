e2c4d8e83cb0bd6ca18e43326ae90b1ad2e8feb9
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-08-14 23:25:38 +0900

    _:(´ཀ`」 ∠):_

--- main.php
+++ main.php
@@ -1,10 +1,14 @@
 <html>
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript charset=utf-8">
+
 <title>掲示板</title>
 <?php
 	require_once "C:/xampp/htdocs/club/bulletin_board/sql.php";
+	require_once "C:/xampp/htdocs/club/bulletin_board/data_process.php";
 ?>
+<script type="text/javascript" src="json.js"></script>
 </head>
 <body>
 	<h1>掲示板ʅ（‾◡◝）ʃ</h1>
@@ -28,12 +32,31 @@
 				return false;
 			}
 		}
+		
 	</script>
 	<form name="mainBBS" method="post" action="" onSubmit="return check()">
 		名前 : <input type="text" name="name" value="">&nbsp<br>
 		<input type="text" name="comment" size="50" value="ここに書き込んでください"><br>
 		<input type="submit" name="submit" value="提出">
 	</form>
-
+	<?php
+		$conn = new MySQL("localhost","bbs","ferret","ferret");
+		$sql = "SELECT name,text,date FROM bbs";
+		$result = $conn->fetch($sql);
+		showdata($result);
+	?>
+	<script>
+	/*
+		var promiseobj = loadJSON("http://localhost/club/bulletin_board/json.php");
+		promiseobj.then(function(data){
+			//for(var i = 0;i < 1;i++){
+				alert("名前 : "+data[0].name+"<br>");
+				document.write("内容 : "+data[0].text+"<br>");
+				document.write("日付 : "+data[0].date+"<br>");
+			//}
+		});
+	*/
+	</script>
+	
 </body>
 </html>
