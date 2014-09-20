cbc5014e54f3ea6e715de7bfffe1f063e69455a6
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-09-21 00:40:44 +0900

    check data

---
+++ registration.js
@@ -0,0 +1,70 @@
+	function getdata(id){
+		return document.getElementById(id).value;
+	}
+	
+	function radio(){
+		var i;
+		for(i=0;i<document.form.sex.length;i++){
+			if(document.form.sex[i].checked){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	function datacheck(){
+		var flg = 0;
+		var userid = getdata('userid');
+		var username = getdata('username');
+		var pass = getdata('userpass');
+		var email = getdata('email');
+		var old = getdata('old');
+		var birth = getdata('birthday');
+		if(userid == ""){
+			document.getElementById('uid').innerHTML = "un-answered userid";
+			flg = 1;
+		}else{
+			document.getElementById('uid').innerHTML = "";
+		}
+		if(username == ""){
+			document.getElementById('uname').innerHTML = "un-answered username";
+			flg = 1;
+		}else{
+			document.getElementById('uname').innerHTML = "";
+		}
+		if(pass == ""){
+			document.getElementById('upass').innerHTML = "un-answered password";
+			flg = 1;
+		}else{
+			document.getElementById('upass').innerHTML = "";
+		}
+		if(email == ""){
+			document.getElementById('uemail').innerHTML = "un-answered email";
+			flg = 1;
+		}else{
+			document.getElementById('uemail').innerHTML = "";
+		}
+		if(old == ""){			
+			document.getElementById('uold').innerHTML = "un-answered old";
+			flg = 1;
+		}else{
+			document.getElementById('uold').innerHTML = "";
+		}
+		if(birth == ""){
+			document.getElementById('ubirth').innerHTML = "un-answered birthday";
+			flg = 1;
+		}else{
+			document.getElementById('ubirth').innerHTML = "";
+		}
+		if(!radio()){
+			document.getElementById('usex').innerHTML = "un-answered sex";
+			flg = 1;
+		}else{
+			document.getElementById('usex').innerHTML = "";
+		}
+		if(flg == 1){
+			return false;
+		}else{
+			return true;
+		}
+	}
