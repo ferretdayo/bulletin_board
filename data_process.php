d1da5520fb0321b52a9316555fcc86a3fc38910b
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-09-21 00:44:27 +0900

    Delete処理やSessionの処理も追加

--- data_process.php
+++ data_process.php
@@ -1,7 +1,26 @@
 <?php
-	require_once "C:/xampp/htdocs/club/bulletin_board/sql.php";
-	$conn = new MySQL("","","","");
-	$sql = "INSERT INTO bbs VALUES(:id,:name,:text,:date)";
-	$array = array(':id' => '1',':name' => 'ferret',':text' => '初投稿でーす♡',':date' => '2014/08/07');
-	$result = $conn->insert($sql,$array);
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$conn = new MySQL("localhost","bbs","ferret","ferret");
+		//INSERT
+		if(isset($_POST["submit"])){
+			if(isset($_SESSION["login"])){
+				if($_SESSION["login"] == true){
+					$name = $_SESSION["userid"];
+				}
+			}else{
+				exit();
+			}
+			$text = htmlspecialchars($_POST["comment"]);
+			$date = date('Y/m/d');
+			$sql = "INSERT INTO bbs(userid,text,date) VALUES(:userid,:text,:date)";
+			$array = array(':userid' => $name,':text' => $text,':date' => $date);
+			$conn->insert($sql,$array);
+		}
+		if(isset($_POST["delete"])){
+			var_dump($_POST["delete"]);
+			$no = key($_POST["delete"]);
+			$sql = "DELETE FROM bbs WHERE id = {$no}";
+			$conn->delete($sql);
+		}
+	}
 ?>
