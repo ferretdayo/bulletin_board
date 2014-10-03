2d5e2866d54cfc18d8b31abf89a90b6997f3a63e
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-03 10:41:06 +0900

    used Bootstrap

--- login.php
+++ login.php
@@ -5,49 +5,48 @@
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
 <title>Login</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+	<div style="padding:20px 0;">
+		<div class="container">
+			<h1>Login Page</h1>
 <?php
-	require_once "C:/xampp/htdocs/club/bulletin_board/sql.php";
+	require_once "./sql.php";
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		if(isset($_POST["login"])){
-			$userid = htmlspecialchars($_POST["username"]);
+			$userid = htmlspecialchars($_POST["userid"]);
 			$userpass = htmlspecialchars($_POST["userpass"]);
 			$conn = new MySQL("localhost","bbs","ferret","ferret");
 			$array = array(':userid' => $userid,':userpass' => $userpass);
 			$sql = "SELECT userid,userpass FROM registration where userid = :userid AND userpass = :userpass";
 			$result = $conn->fetch($sql,$array);
 			if(!$result){	//false
-				echo "You mistake userid and userpassword";
+				echo "<p style='color:red'>You mistake userid and userpassword</p>";
 			}else{			//true
 				//web don't have session.
-				if(!isset($_SESSION["login"])){
-					$_SESSION["login"] = true;
-					$_SESSION["userid"] = $result['userid'];
-				}else{	//web has session
-					//if user could logined.
-					if($_SESSION["login"] == true){
-						//redirect
-						header("Location: ./main.php");
-					}else{
-						$_SESSION = array();
-						if (isset($_COOKIE["PHPSESSID"])) {
-							setcookie("PHPSESSID", '', time() - 1800, '/');
-						}
-						session_destroy();
-					}
-				}
+				$_SESSION["login"] = true;
+				$_SESSION["userid"] = $result['userid'];
+				header("Location: ./main.php");
 			}
 		}
 	}
 ?>
-	<form action="" method="post">
-		<table>
-			<tr><td>UserName : </td><td><input type="text" name='username'></td></tr>
-			<tr><td>UserPassword : </td><td><input type="password" name="userpass"></td></tr>
-			<tr><td>&nbsp;</td><td><input type="submit" name="login"></td></tr>
-		</table>
-		<a href="./registration.php">create user account</a>
-	</form>
+			<form action="" method="post" class="form-horizontal" style="margin-bottom:15px;">
+				<div class="form-group">
+						<label class="control-label" for="UserID">UserID</label>
+						<input type="text" class="form-control" placeholder="userid" id='userid' name='userid'>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="password">UserPassword</label>
+					<input type="password" class="form-control" placeholder="password" id="userpass" name="userpass"><div id='upass'></div>
+				</div>
+				<div class="form-inline">
+						<input type="submit" name="login" value="Login" class="btn btn-primary">
+						<a href="./registration.html" class="btn btn-link">create user account</a>
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
