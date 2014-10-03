1c73e9741f9c1f93a519339aadbee4d7aa8473a5
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-03 10:46:23 +0900

    updata

--- data_process.php
+++ data_process.php
@@ -12,12 +12,17 @@
 			}
 			$text = htmlspecialchars($_POST["comment"]);
 			$date = date('Y/m/d');
-			$sql = "INSERT INTO bbs(userid,text,date) VALUES(:userid,:text,:date)";
-			$array = array(':userid' => $name,':text' => $text,':date' => $date);
+			$sql = "SELECT MAX(comment) AS comment_max FROM bbs";
+			$result = $conn->fetch($sql);
+			if($result){
+				$comment_max = $result[0]['comment_max'];
+				$comment_max++;
+			}
+			$sql = "INSERT INTO bbs(userid,text,date,comment) VALUES(:userid,:text,:date,:comment)";
+			$array = array(':userid' => $name,':text' => $text,':date' => $date,':comment' => $comment_max);
 			$conn->insert($sql,$array);
 		}
 		if(isset($_POST["delete"])){
-			var_dump($_POST["delete"]);
 			$no = key($_POST["delete"]);
 			$sql = "DELETE FROM bbs WHERE id = {$no}";
 			$conn->delete($sql);
