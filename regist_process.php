2f86dbe7c3b02eca6812d747b96f7caf14b9bf55
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-08 00:15:40 +0900

    Update regist_process.php

--- regist_process.php
+++ regist_process.php
@@ -11,17 +11,16 @@
 	require_once "./sql.php";
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		if(isset($_POST['confirm'])){
-			$conn = new MySQL("localhost","bbs","ferret","ferret");
+			$conn = new MySQL("","","","");
 			$userid = htmlspecialchars($_POST['userid']);
 			$username = htmlspecialchars($_POST['username']);
 			$userpass = htmlspecialchars($_POST['userpass']);
 			$email = htmlspecialchars($_POST['email']);
-			$old = htmlspecialchars($_POST['old']);
 			$sex = $_POST['sex'];
 			$birthday = $_POST['birthday'];
-			$sql = "INSERT INTO registration(userid,name,email,sex,old,birth,userpass)";
-			$sql .= "VALUES(:userid,:name,:email,:sex,:old,:birth,:userpass)";
-			$array = array(':userid'=>$userid,':name'=>$username,':email'=>$email,':sex'=>$sex,':old'=>$old,':birth'=>$birthday,':userpass'=>$userpass);
+			$sql = "INSERT INTO registration(userid,name,email,sex,birth,userpass)";
+			$sql .= "VALUES(:userid,:name,:email,:sex,:birth,:userpass)";
+			$array = array(':userid'=>$userid,':name'=>$username,':email'=>$email,':sex'=>$sex,':birth'=>$birthday,':userpass'=>$userpass);
 			$conn->insert($sql,$array);
 			echo "Create user!<br>";
 		}
