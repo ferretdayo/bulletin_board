a0eaef3bec81987d23a08888a8e15520cb52863b
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-08 00:11:28 +0900

    コメントの場合セッションに値を登録

--- data_process.php
+++ data_process.php
@@ -1,31 +1,36 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$conn = new MySQL("localhost","bbs","ferret","ferret");
+		$conn = new MySQL("","","","");
 		//INSERT
 		if(isset($_POST["submit"])){
 			if(isset($_SESSION["login"])){
 				if($_SESSION["login"] == true){
-					$name = $_SESSION["userid"];
+					$userid = $_SESSION["userid"];
 				}
 			}else{
 				exit();
 			}
-			$text = htmlspecialchars($_POST["comment"]);
+			$text = htmlspecialchars($_POST["text_comment"]);
 			$date = date('Y/m/d');
-			$sql = "SELECT MAX(comment) AS comment_max FROM bbs";
-			$result = $conn->fetch($sql);
-			if($result){
-				$comment_max = $result[0]['comment_max'];
-				$comment_max++;
-			}
-			$sql = "INSERT INTO bbs(userid,text,date,comment) VALUES(:userid,:text,:date,:comment)";
-			$array = array(':userid' => $name,':text' => $text,':date' => $date,':comment' => $comment_max);
+			$sql = "INSERT INTO bbs(userid,text,date) VALUES(:userid,:text,:date)";
+			$array = array(':userid' => $userid,':text' => $text,':date' => $date);
 			$conn->insert($sql,$array);
 		}
 		if(isset($_POST["delete"])){
 			$no = key($_POST["delete"]);
 			$sql = "DELETE FROM bbs WHERE id = {$no}";
 			$conn->delete($sql);
+		}
+		if(isset($_POST["comment"])){
+			$bbsid = key($_POST["comment"]);
+			$sql = "SELECT id FROM bbs WHERE id = :bbsid";
+			$data = array(':bbsid'=>$bbsid);
+			$result = $conn->fetch($sql,$data);
+			if($result){
+				$bbs_id = $result[0]['id'];	//get bbs id
+				$_SESSION["bbs_id"] = $bbs_id;
+			}
+			header("Location: ./comment.php");
 		}
 	}
 ?>
