011f0fbeffd04099f1c923b31f2c25c7ab4d0163
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-13 12:17:47 +0900

    Sessionの期限を追加、comment数の取得

--- json.php
+++ json.php
@@ -4,11 +4,23 @@
 	session_start();
 	$userid = $_SESSION["userid"];
 	$namearray = array('userid'=>$userid);
+	$idarray = array();
 	header("Content-Type: application/json; charset=utf-8");
 	
-	$conn = new MySQL("","","","");
+	$conn = new MySQL("localhost","bbs","ferret","ferret");
+	//Get Time Line
 	$sql = "SELECT bbs.id,bbs.userid,bbs.text,bbs.date,registration.name FROM bbs INNER JOIN registration ON bbs.userid = registration.userid";
 	$result = $conn->fetch($sql);
+	$i = 0;
+	foreach($result as $value){
+		$query = "SELECT count(bbs_id) FROM bbs_comment WHERE bbs_id = :id";
+		$array = array(':id' => $value['id']);
+		$ans = $conn->fetch($query,$array);
+		foreach($ans as $value){
+			array_push($idarray,$value['count(bbs_id)']);
+		}
+	}
+	array_unshift($result,$idarray);
 	array_unshift($result,$namearray);
 	$json_data = json_encode($result);
 	echo trim($json_data);
