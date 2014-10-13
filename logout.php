49b5b2f07239611d3cfdf8c00fc96ec78287d2f4
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-13 12:18:40 +0900

    修正

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
 <?php
+	session_start();
 	$_SESSION = array();
 	if (isset($_COOKIE["PHPSESSID"])) {
-		setcookie("PHPSESSID", '', time() - 1800, '/');
+		setcookie("PHPSESSID", '', time() - 3600, '/');
 	}
 	session_destroy();
 	header("Location: ./login.php");
