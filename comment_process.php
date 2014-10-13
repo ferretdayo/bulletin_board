c552c96b4101396224ac67f07444d8f9dd1e2deb
ふぇれっと(Tomohiro) <ferretdayo@hotmail.co.jp>
2014-10-13 12:27:18 +0900

    Sessionの期限を追加

--- comment_process.php
+++ comment_process.php
@@ -7,8 +7,13 @@
 				if($_SESSION["login"] == true){
 					$userid = $_SESSION["userid"];
 					$bbs_id = $_SESSION["bbs_id"];
+					$_SESSION["last"] = time();
 				}
 			}else{
+				echo "Please Login";
+				echo <<<EOF
+				<input type='button' value='Go back LoginPage' class="btn btn-primary" onclick='location.href="./login.php"'>
+EOF;
 				exit();
 			}
 			$text = htmlspecialchars($_POST["comment"]);
@@ -18,6 +23,9 @@
 			$conn->insert($sql,$array);
 		}
 		if(isset($_POST["delete"])){
+			if(isset($_SESSION["login"])){
+				$_SESSION["last"] = time();
+			}
 			$no = key($_POST["delete"]);
 			$sql = "DELETE FROM bbs_comment WHERE id = {$no}";
 			$conn->delete($sql);
